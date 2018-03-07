89263bcc9cee125d4756cc1487a5522bf453603b
Eugen-Chervinskiy <zekakizaru5@gmail.com>
2018-03-07 09:51:38 +0200

    new functions

--- OOPControlApplication/DataParser.cs
+++ OOPControlApplication/DataParser.cs
@@ -18,31 +18,31 @@ namespace OOPControlApplication
             return category;
         }
 
-        public string GetFileName()
+        public string GetFileName(string text)
         {
             string fileName = "";
             return fileName;
         }
 
-        public string GetExtension()
+        public string GetExtension(string text)
         {
             string extension = "";
             return extension;
         }
 
-        public string GetSize()
+        public string GetSize(string text)
         {
             string size = "";
             return size;
         }
 
-        public string GetResolution()
+        public string GetResolution(string text)
         {
             string resolution = "";
             return resolution;
         }
 
-        public string GetDuration()
+        public string GetDuration(string text)
         {
             string duration = "";
             return duration;
